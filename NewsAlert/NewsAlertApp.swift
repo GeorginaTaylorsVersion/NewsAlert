a64ff5fe65a9b0abb3ecc6729eabb911e9f0817b
Georgina <georgina.wang007@gmail.com>
2026-02-16 22:05:39 -0500

    Initial Commit

---
+++ NewsAlert/NewsAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAlertApp.swift
+//  NewsAlert
+//
+//  Created by Georgina on 2026-02-16.
+//
+
+import SwiftUI
+
+@main
+struct NewsAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
